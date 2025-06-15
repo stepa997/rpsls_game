3d863f5b4d5c9fbba37f83306ae8bdad51d4062b
Djordje Stepanovic <djordje.stepanovic@aspiretechnology.com>
2025-06-15 13:37:51 +0200

    Change pytest connection leak

--- rpsls_game_api/tests/test_connection_leak.py
+++ rpsls_game_api/tests/test_connection_leak.py
@@ -1,18 +1,19 @@
 # test_connection_leak.py
 import gc
 import pytest
+from sqlalchemy import text
 
 
 def test_no_connection_leak(session):
     # Open and close 100 connections
     for _ in range(100):
-        session.execute("SELECT 1")
+        session.execute(text("SELECT 1"))
 
     # gc collect
     gc.collect()
 
     # If leak connection this is fail
     try:
-        session.execute("SELECT 1")
+        session.execute(text("SELECT 1"))
     except Exception as e:
         pytest.fail(f"Connection leak detected: {e}")
